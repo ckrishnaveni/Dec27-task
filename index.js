e2d0690720adb2b66b747a3ff5b18c1077ac0828
krishnaveni <krishnaveni042@gmail.com>
2024-12-27 17:31:37 +0530

    reverse values in array

---
+++ index.js
@@ -0,0 +1,27 @@
+let arr3=["hello","welcome","to","javascript"]
+let arr=[];
+for (i=0;i<arr3.length;i++){
+let str="";
+for(j=arr3[i].length-1;j>=0;j--){
+str += arr3[i][j];
+}
+arr[i]=str;
+}
+console.log(arr);
+
+
+// let arr2=["hello","welcome","to","javascript"]
+// let arr=[];
+// for(let i=0;i<arr2.length;i++){
+//    let str="";
+//    if (i % 2 ! == 0){
+//     for(j=arr2[i].length-1;j>=0;j--){
+//         str += arr2[i][j];
+//     }
+// }
+// else{
+//     str=arr2[i];
+// }
+// arr[i=str];
+// }
+// console.log(arr);
