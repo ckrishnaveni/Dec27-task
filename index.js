410138350a088fe5f7a9587e427edea73267e9cf
krishnaveni <krishnaveni042@gmail.com>
2024-12-30 10:43:57 +0530

    reversed array values & also reversed value only for odd index's

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ arr[i]=str;
 console.log(arr);
 
 
-// let arr2=["hello","welcome","to","javascript"]
-// let arr=[];
-// for(let i=0;i<arr2.length;i++){
-//    let str="";
-//    if (i % 2 ! == 0){
-//     for(j=arr2[i].length-1;j>=0;j--){
-//         str += arr2[i][j];
-//     }
-// }
-// else{
-//     str=arr2[i];
-// }
-// arr[i=str];
-// }
-// console.log(arr);
+let arr2=["hello","welcome","to","javascript"]
+let arr1=[];
+for(let i=0;i<arr2.length;i++){
+   let str="";
+   if (i%2==0){
+    for(j=arr2[i].length-1;j>=0;j--){
+        str += arr2[i][j];
+    }
+}
+else {
+    str=arr2[i];
+}
+arr1[i]=str;
+}
+console.log(arr1);
